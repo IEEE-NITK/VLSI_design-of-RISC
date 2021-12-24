de259183802aa2c33fd94f551282a5157d9324a5
anirudh1221 <78913275+anirudh1221@users.noreply.github.com>
2021-12-25 01:00:59 +0530

    Update test_bench_memory.v

--- verilog/tb/test_bench_memory.v
+++ verilog/tb/test_bench_memory.v
@@ -5,29 +5,27 @@ module tb;
   wire [31:0] read_data;
   reg [31:0] address;
   reg [31:0] write_data;
-  reg MemWrite,MemRead,clk;
+  reg MemWrite, MemRead, clk;
  
   
-  memory_unit C0 (address, write_data, read_data, MemWrite, MemRead, clk);
+  memory_unit C0 (read_data, address, write_data, MemWrite, MemRead, clk);
 
   initial begin
-	clk=0;
-	forever #5 clk=~clk;
+    clk=0;
+    forever #5 clk=~clk;
   end
-
-
 
   initial begin
     #1  
     #10 address=10'b00001010; write_data=32'h000001;MemWrite=1'b1; MemRead=1'b0; //write data
-    #10 MemRead= 1'b1; MemWrite=1'b0; //read data
+    #10  MemRead= 1'b1; MemWrite=1'b0; //read data
     #10 MemWrite=1'b0; MemRead= 1'b1;
     #10 $finish;
   end
   
   initial begin
-  	$dumpfile("dump.vcd"); 
-        $dumpvars;
+    $dumpfile("dump.vcd"); 
+    $dumpvars;
   end
 
 endmodule
